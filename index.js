ff4b365e9977709c89f08e89c3a71f6fea67144a
Hadas Gottlieb <hadasgottlieb194@gmail.com>
2020-05-12 03:29:13 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    console.log("function ran");
+    if (this.readyState == 4 && this.status == 200) {
+
+    let response = JSON.parse(xhttp.responseText);
+    let merchandise = response.merchandise;
+    let output = ``;
+    for (let i = 0; i < merchandise.length; i++) {
+        output += `<li class = "item"><img src="${merchandise[i].photo}" alt="ring photo" height=250 width=auto><br><span class = "item-text1 item-text">${merchandise[i].name}</span><br><span class = "item-text2 item-text"> ${merchandise[i].name2}</span><br><button>View Details</button></li> `
+    }
+    $(".item-list").html(output);
+    
+    }
+};
+xhttp.open("GET", "test.json", true);
+xhttp.send();
