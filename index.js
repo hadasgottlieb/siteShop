2e8e3d9dd0291a138e47c8fd9643de2f62910c77
Hadas Gottlieb <hadasgottlieb194@gmail.com>
2020-05-25 11:27:10 -0400

    added lightbox

--- index.js
+++ index.js
@@ -5,20 +5,36 @@ xhttp.onreadystatechange = function() {
 
     let response = JSON.parse(xhttp.responseText);
     let merchandise = response.merchandise;
-    // let output = ``;
-    $(".classic").on("click", function() {
+
+
+
+    function collectClassic() {
         console.log("classic ran");
-        let output = ``;
-        $(".merch-title").html("Classic");
-        for (let i = 0; i < merchandise.length; i++) {
-            if (merchandise[i].classic === true) {
-            
-            output += `<li data-item-id = "${merchandise[i].code}" class = "item"><img src="${merchandise[i].photo}" alt="${merchandise[i].name} photo" height=250 width=auto><br><span class = "item-text1 item-text">${merchandise[i].name}</span><br><span class = "item-text2 item-text"> ${merchandise[i].name2}</span><br><button data-item-id = "${merchandise[i].code}" class = "button-lightbox">View Details</button></li> `
+            let output = ``;
+            $(".merch-title").html("Classic Collection");
+            for (let i = 0; i < merchandise.length; i++) {
+                if (merchandise[i].classic === true) {
+                
+                output += `<li data-item-id = "${merchandise[i].code}" class = "item"><img src="${merchandise[i].photo}" alt="${merchandise[i].name} photo" height=250 width=auto><br><span class = "item-text1 item-text">${merchandise[i].name}</span><br><span class = "item-text2 item-text"> ${merchandise[i].name2}</span><br><button data-item-id = "${merchandise[i].code}" class = "button-ltbox">View Details</button></li> `
+                }
             }
-        }
-        $(".item-list").empty();
-        $(".item-list").html(output);
-    });
+            $(".item-list").empty();
+            $(".item-list").html(output);
+    };
+        $(".classic").on("click", function() {
+            console.log("classic ran");
+            let output = ``;
+            $(".merch-title").html("Classic Collection");
+            for (let i = 0; i < merchandise.length; i++) {
+                if (merchandise[i].classic === true) {
+                
+                output += `<li data-item-id = "${merchandise[i].code}" class = "item"><img src="${merchandise[i].photo}" alt="${merchandise[i].name} photo" height=250 width=auto><br><span class = "item-text1 item-text">${merchandise[i].name}</span><br><span class = "item-text2 item-text"> ${merchandise[i].name2}</span><br><button data-item-id = "${merchandise[i].code}" class = "button-ltbox">View Details</button></li> `
+                }
+            }
+            $(".item-list").empty();
+            $(".item-list").html(output);
+        });
+    
     $(".bands").on("click", function() {
         console.log("bands ran");
         let output = ``;
@@ -26,7 +42,7 @@ xhttp.onreadystatechange = function() {
         for (let i = 0; i < merchandise.length; i++) {
             if (merchandise[i].bands === true) {
             
-            output += `<li data-item-id = "${merchandise[i].code}" class = "item"><img src="${merchandise[i].photo}" alt="${merchandise[i].name} photo" height=250 width=auto><br><span class = "item-text1 item-text">${merchandise[i].name}</span><br><span class = "item-text2 item-text"> ${merchandise[i].name2}</span><br><button data-item-id = "${merchandise[i].code}" class = "button-lightbox">View Details</button></li> `
+            output += `<li data-item-id = "${merchandise[i].code}" class = "item"><img src="${merchandise[i].photo}" alt="${merchandise[i].name} photo" height=250 width=auto><br><span class = "item-text1 item-text">${merchandise[i].name}</span><br><span class = "item-text2 item-text"> ${merchandise[i].name2}</span><br><button data-item-id = "${merchandise[i].code}" class = "button-ltbox">View Details</button></li> `
             }
         }
         $(".item-list").empty();
@@ -35,12 +51,12 @@ xhttp.onreadystatechange = function() {
     $(".halo").on("click", function() {
         console.log("halo ran");
         let output = ``;
-        $(".merch-title").html("Halo");
+        $(".merch-title").html("Halo Rings");
         for (let i = 0; i < merchandise.length; i++) {
             
             if (merchandise[i].halo === true) {
                 
-                output += `<li data-item-id = "${merchandise[i].code}" class = "item"><img src="${merchandise[i].photo}" alt="${merchandise[i].name} photo" height=250 width=auto><br><span class = "item-text1 item-text">${merchandise[i].name}</span><br><span class = "item-text2 item-text"> ${merchandise[i].name2}</span><br><button data-item-id = "${merchandise[i].code}" class = "button-lightbox">View Details</button></li> `
+                output += `<li data-item-id = "${merchandise[i].code}" class = "item"><img src="${merchandise[i].photo}" alt="${merchandise[i].name} photo" height=250 width=auto><br><span class = "item-text1 item-text">${merchandise[i].name}</span><br><span class = "item-text2 item-text"> ${merchandise[i].name2}</span><br><button data-item-id = "${merchandise[i].code}" class = "button-ltbox">View Details</button></li> `
             }
         }
         $(".item-list").empty();
@@ -49,12 +65,12 @@ xhttp.onreadystatechange = function() {
     $(".curlique").on("click", function() {
         console.log("curlique ran");
         let output = ``;
-        $(".merch-title").html("Curlique");
+        $(".merch-title").html("Curlique Collection");
         for (let i = 0; i < merchandise.length; i++) {
             $(".item-list").html("");
             if (merchandise[i].curlique === true) {
                 
-                output += `<li data-item-id = "${merchandise[i].code}" class = "item"><img src="${merchandise[i].photo}" alt="${merchandise[i].name} photo" height=250 width=auto><br><span class = "item-text1 item-text">${merchandise[i].name}</span><br><span class = "item-text2 item-text"> ${merchandise[i].name2}</span><br><button data-item-id = "${merchandise[i].code}" class = "button-lightbox">View Details</button></li> `
+                output += `<li data-item-id = "${merchandise[i].code}" class = "item"><img src="${merchandise[i].photo}" alt="${merchandise[i].name} photo" height=250 width=auto><br><span class = "item-text1 item-text">${merchandise[i].name}</span><br><span class = "item-text2 item-text"> ${merchandise[i].name2}</span><br><button data-item-id = "${merchandise[i].code}" class = "button-ltbox">View Details</button></li> `
             }
         }
         $(".item-list").empty();
@@ -63,29 +79,47 @@ xhttp.onreadystatechange = function() {
     $(".custom").on("click", function() {
         console.log("custom ran");
         let output = ``;
-        $(".merch-title").html("Custom");
+        $(".merch-title").html("Custom-made");
         for (let i = 0; i < merchandise.length; i++) {
             if (merchandise[i].custom === true) {
                 $(".item-list").empty();
-                output += `<li data-item-id = "${merchandise[i].code}" class = "item"><img src="${merchandise[i].photo}" alt="${merchandise[i].name} photo" height=250 width=auto><br><span class = "item-text1 item-text">${merchandise[i].name}</span><br><span class = "item-text2 item-text"> ${merchandise[i].name2}</span><br><button data-item-id = "${merchandise[i].code}" class = "button-lightbox">View Details</button></li> `
+                output += `<li data-item-id = "${merchandise[i].code}" class = "item"><img src="${merchandise[i].photo}" alt="${merchandise[i].name} photo" height=250 width=auto><br><span class = "item-text1 item-text">${merchandise[i].name}</span><br><span class = "item-text2 item-text"> ${merchandise[i].name2}</span><br><button data-item-id = "${merchandise[i].code}" class = "button-ltbox">View Details</button></li> `
             }
         }
         $(".item-list").empty();
         $(".item-list").html(output);
     });
 
-           $(".button-lightbox").on("click", function() {
-            console.log(`lightbox ran`);
-            let ident = $(this).data("item-id");
+    function ltbox() {
+        //on click view details, ltbox is emptied, and ltbox container toggles to display: block, and clicked item info is displayed in ltbox. 
+        $(".item-list").on("click", ".item .button-ltbox", function() {
+            console.log("ltbox click function ran");
+            $(".ltbox-content").empty(); //should delete previously displayed item info
+            $(".ltbox-container").toggleClass("display-off"); //should turn on lightbox
+
+            let ident = $(this).data("item-id"); //that's the id of the li element
             console.log(ident);
-            const lightboxRing = merchandise.find(lightboxRing => lightboxRing.code === ident);
-            $(".lightbox").html(`<img src = "${lightboxRing.photo}" height = 300 width = auto><span>${lightboxRing.name}</span><span>${lightboxRing.name2}</span><span>${lightboxRing.code}</span><span>${lightboxRing.price}</span>`);
-    
+            const item = merchandise.find(item => item.code === ident);
+            $(".ltbox-content").append(`<img src="${item.photo}" alt="${item.name}" height=300 width=300>`);
+            $(".ltbox-content").append(`<div><span class = "item-text3">${item.name}</span><br><span class = "item-text4">${item.name2}</span><br><br><span>${item.code}</span><br><span>${item.price}</span></div>`);
+
         });
+    };     
+
+    function ltboxClose() {
+        console.log("ltboxClose ran");
+        //on click X, ltbox container toggles to display: none;
+        $(".ltbox-close").on("click", function() {
+            $(".ltbox-container").toggleClass("display-off"); //should turn off lightbox
+        });
+    }
     
     
     
     }
+    $(collectClassic);
+    $(ltbox);
+    $(ltboxClose);
 };
 xhttp.open("GET", "test.json", true);
 xhttp.send();
